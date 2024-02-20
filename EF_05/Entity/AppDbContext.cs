5fba6fbe2dbf772706d363c365defa466d51b6ba
Pilz2002 <102512475+Pilz2002@users.noreply.github.com>
2024-02-20 16:24:08 +0700

    init

---
+++ EF_05/Entity/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF_05.Entity
+{
+    public class AppDbContext:DbContext
+    {
+        public virtual DbSet<CongThuc> CongThuc { get; set; }
+        public virtual DbSet<LoaiMonAn> LoaiMonAn { get; set; }
+        public virtual DbSet<MonAn> MonAn { get; set; }
+        public virtual DbSet<NguyenLieu> NguyenLieu { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server = QUYEN; Database = QLMonAn; Trusted_Connection = True;" +
+				"TrustServerCertificate=True");
+        }
+    }
+}
